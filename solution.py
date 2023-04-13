02073505e37e9aaaf18e069097d65463e6fe268a
yaroslav-yarik <86802498+yaroslav-yarik@users.noreply.github.com>
2023-04-13 22:12:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import norm
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 561049466
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+  
+    p_comb = (x_success + y_success) / (x_cnt + y_cnt)
+    p_value = 1 - norm.cdf((y_success / y_cnt - x_success / x_cnt) / np.sqrt(p_combined * (1 - p_combined) * (1 / x_cnt + 1 / y_cnt)))
+    return p_value < 0.07
